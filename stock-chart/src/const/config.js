f8ada436f7df59956b8718945ec3969ffe1bd8cb
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-01-17 15:18:02 -0800

    tailwind and API info added

---
+++ stock-chart/src/const/config.js
@@ -0,0 +1,7 @@
+export const chartConfig = {
+    "1H": {hours: 1 , days: 0, weeks: 0, months: 0, years: 0, resolustion: ""},
+    "1D": {hours: 0 , days: 1, weeks: 0, months: 0, years: 0, resolustion: ""},
+    "1W": {hours: 0 , days: 0, weeks: 1, months: 0, years: 0, resolustion: ""},
+    "1M": {hours: 0 , days: 0, weeks: 0, months: 1, years: 0, resolustion: ""},
+    "1y": {hours: 0 , days: 0, weeks: 0, months: 0, years: 1, resolustion: ""},
+}
